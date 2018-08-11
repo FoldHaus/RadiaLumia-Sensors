32d33bbad9c1a10c5408177d69ca366bb7f62f7a
Nathalie Collins <nathalie@ideo.org>
2018-08-11 15:44:30 -0700

    tested with ifdef DEBUG

--- RadiaLumia-Ladder/RadiaLumia-Ladder.ino
+++ RadiaLumia-Ladder/RadiaLumia-Ladder.ino
@@ -19,6 +19,8 @@
 
 // ------------------ Configuration for the load cells ------------------
 
+#define DEBUG                       // uncomment when you want to output info to the console
+
 #define calibration_factor 10500.0  // Make sure to let it start up with base weight already hanging
 #define CHECK_INTERVAL 500          // Delay in ms between sensor checks
 #define STAY_OPEN 10000             // Keep the entrance open for 10 seconds after receiving a reading
@@ -29,7 +31,7 @@
 
 #define DOUT1  6                    // load cell 1 data input
 #define DOUT2  9                    // load cell 2 data input
-#define DOUT3  4                   // load cell 3 data input
+#define DOUT3  4                    // load cell 3 data input
 #define CLK    5
 
 HX711 scale1(DOUT1, CLK);
@@ -49,12 +51,10 @@ int packetNum = 0;      // packet number for testing only
 float paramValue = 0;
 
 IPAddress lxServer(192,168,1,10);
-//IPAddress lxServer(192,168,42,65);  // Nathalie home
 int lxPort = 7878;
 
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192,168,1,11);
-//IPAddress ip(192, 168, 42, 200);    // Nathalie home
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
@@ -86,9 +86,12 @@ EthernetUDP Udp;
 
 void setup() {
   // ------------------ Setup for load cells ------------------
-  
-  Serial.begin(9600);  
+
+#ifdef DEBUG
+  Serial.begin(9600); 
+  delay(1000); 
   Serial.println("Starting up our three load cells");
+#endif
 
   scale1.set_scale(calibration_factor); // assign calibration factor
   scale1.tare();  // Assuming there is no weight on the scale at start up, reset the scale to 0
@@ -99,7 +102,9 @@ void setup() {
   scale3.set_scale(calibration_factor);
   scale3.tare();
 
+#ifdef DEBUG
   Serial.println("Readings:");
+#endif
 
   // ------------------ Setup for networking ------------------
   
@@ -116,22 +121,26 @@ void setup() {
 //  while (!Serial); // wait for serial port to connect.
 #endif
 
+#ifdef DEBUG
   Serial.println("\nHello! I am the Ethernet FeatherWing");
+#endif
 
+  // start the ethernet module and give it time to boot up
   Ethernet.init(WIZ_CS);
-  
-  // give the ethernet module time to boot up:
   delay(1000);
 
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
+#ifdef DEBUG
     Serial.println("Failed to configure Ethernet using DHCP");
+#endif
     // no point in carrying on, so do nothing forevermore:
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
   }
   Udp.begin(8888);
-  
+
+#ifdef DEBUG
   // print the Ethernet board/shield's IP address:
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
@@ -140,24 +149,28 @@ void setup() {
   Serial.println(lxServer);
   Serial.print("LX port: ");
   Serial.println(lxPort);
+  Serial.println();
+#endif
 
   oscMessage(CLOSED);  // zero the ladder sensor with LX when we start up
+#ifdef DEBUG
   Serial.println("Sent 0 to LX to get it started");
+#endif
 }
 
 // LOOP STARTS HERE
 
 void loop() {
-  Serial.print("Reading: ");
-
+#ifdef DEBUG
+  Serial.print("\nReading: ");
   Serial.print(scale1.get_units(), 1); // scale.get_units() returns a float
   Serial.print(" ");
   Serial.print(scale2.get_units(), 1);
   Serial.print(" ");
   Serial.print(scale3.get_units(), 1);
-
   Serial.print(" lbs"); // You can change this to kg but you'll need to refactor the calibration_factor
   Serial.println();
+#endif
 
   // trigger the sensor team if at least one of the load cells is over weight
   int triggered = 0;
@@ -169,24 +182,29 @@ void loop() {
   if (triggered) {
     setNewState(OPEN);
     stayOpenCounter = 0;
+#ifdef DEBUG
     Serial.println("triggered, so open");
+#endif
   }
 
   // if it's already open and it's been longer than the delay, close the entrance
   if (entranceOpen && (stayOpenCounter >= STAY_OPEN)) {
     setNewState(CLOSED);
     stayOpenCounter = 0;
+#ifdef DEBUG
     Serial.println("reset to closed");
+#endif
   }
   // otherwise stay open and increment the counter
   else if (entranceOpen && (stayOpenCounter < STAY_OPEN)) {
     setNewState(OPEN);
     stayOpenCounter += CHECK_INTERVAL;
+#ifdef DEBUG
     Serial.println("stay open and increment counter");
+#endif
   } else {
     setNewState(CLOSED);
   }
-
   delay(CHECK_INTERVAL);  // Check to see if anyone is on the ladder every 2 seconds
 }
 
@@ -200,8 +218,10 @@ void loop() {
 void setNewState(int newState) {
   if (entranceOpen != newState) {
     entranceOpen = newState;
+#ifdef DEBUG
     Serial.print("setting entranceOpen to ");
     Serial.println(entranceOpen);
+#endif
     // oscMessage(newState);      // used when we were only sending a message at state change
   }
   oscMessage(newState);           // send a message with the current state on every loop
@@ -223,12 +243,14 @@ void oscMessage(float paramValue) {
     msg.send(Udp);   
     Udp.endPacket();
 
+#ifdef DEBUG
     // debugging content printed to the Serial
     packetNum++;
     Serial.print("sent packet ");
     Serial.print(packetNum);
     Serial.print(" with value ");
     Serial.println(paramValue);
+#endif
     
     msg.empty(); //free space occupied by message
 }
